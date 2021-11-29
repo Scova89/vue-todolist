8291f445c88a189f73c7aa119b6cc7b724627548
Francesco <scovascova@gmail.com>
2021-11-29 17:31:16 +0100

    input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 const app = new Vue({
+    el: '#root',
+    data: {
+        todos:[
+            {
+                text: 'Portare il cane a spasso',
+                done: true,
+            },
+            {
+                text: 'Fare la spesa',
+                done: true,
+            },
+            {
+                text: 'Preparare la cena',
+                done: true,
+            },        
+        ],
 
-    el: '#root';
+    },
+
+    
+
+    methods: {
+
+    },
 });
